ec0cd8775679f11e4b30a6ae242cdbf789d1e07c
Jennifer <jennifer@ravenwilde.com>
2014-10-13 10:15:19 -0400

    changes to navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -4,13 +4,17 @@
  * Handles toggling the navigation menu for small screens.
  */
 ( function() {
-	var container, button, menu;
+	var header, container, button, menu;
+	
+	header = document.getElementById( 'masthead' );
+	if ( ! header )
+		return;
 
 	container = document.getElementById( 'site-navigation' );
 	if ( ! container )
 		return;
 
-	button = container.getElementsByTagName( 'button' )[0];
+	button = header.getElementsByTagName( 'button' )[0];
 	if ( 'undefined' === typeof button )
 		return;
 
@@ -28,7 +32,9 @@
 	button.onclick = function() {
 		if ( -1 !== container.className.indexOf( 'toggled' ) )
 			container.className = container.className.replace( ' toggled', '' );
+			//button.className = button.className.replace(' is-active', '');
 		else
 			container.className += ' toggled';
+			//button.className += ' is-active';
 	};
 } )();
